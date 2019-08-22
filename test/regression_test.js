c1a791e2302dd0f41d5b6cb94cf1ed9b25e1cd3b
Andrew Waterman <awaterman@salesforce.com>
2019-08-21 17:12:51 -0700

    Revert " use eslint and prettier to apply consistent, modern formatting (#155)" (#167)
    This reverts commit 88618e28dede323d78875b62a6d4d8131296340e.

--- test/regression_test.js
+++ test/regression_test.js
@@ -29,99 +29,95 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
-"use strict";
-const vows = require("vows");
-const assert = require("assert");
-const async = require("async");
-const tough = require("../lib/cookie");
-const Cookie = tough.Cookie;
-const CookieJar = tough.CookieJar;
+'use strict';
+var vows = require('vows');
+var assert = require('assert');
+var async = require('async');
+var tough = require('../lib/cookie');
+var Cookie = tough.Cookie;
+var CookieJar = tough.CookieJar;
 
-const atNow = Date.now();
+var atNow = Date.now();
 
 function at(offset) {
-  return { now: new Date(atNow + offset) };
+  return {now: new Date(atNow + offset)};
 }
 
 vows
-  .describe("Regression tests")
+  .describe('Regression tests')
   .addBatch({
     "Issue 1": {
-      topic: function() {
-        const cj = new CookieJar();
-        cj.setCookie(
-          "hello=world; path=/some/path/",
-          "http://domain/some/path/file",
-          (err, cookie) => {
-            this.callback(err, { cj: cj, cookie: cookie });
-          }
-        );
+      topic: function () {
+        var cj = new CookieJar();
+        cj.setCookie('hello=world; path=/some/path/', 'http://domain/some/path/file', function (err, cookie) {
+          this.callback(err, {cj: cj, cookie: cookie});
+        }.bind(this));
       },
-      "stored a cookie": function(t) {
+      "stored a cookie": function (t) {
         assert.ok(t.cookie);
       },
       "getting it back": {
-        topic: function(t) {
-          t.cj.getCookies("http://domain/some/path/file", (err, cookies) => {
-            this.callback(err, { cj: t.cj, cookies: cookies || [] });
-          });
+        topic: function (t) {
+          t.cj.getCookies('http://domain/some/path/file', function (err, cookies) {
+            this.callback(err, {cj: t.cj, cookies: cookies || []});
+          }.bind(this));
         },
-        "got one cookie": function(t) {
+        "got one cookie": function (t) {
           assert.lengthOf(t.cookies, 1);
         },
-        "it's the right one": function(t) {
-          const c = t.cookies[0];
-          assert.equal(c.key, "hello");
-          assert.equal(c.value, "world");
+        "it's the right one": function (t) {
+          var c = t.cookies[0];
+          assert.equal(c.key, 'hello');
+          assert.equal(c.value, 'world');
         }
       }
     }
   })
   .addBatch({
     "trailing semi-colon set into cj": {
-      topic: function() {
-        const cb = this.callback;
-        const cj = new CookieJar();
-        const ex = "http://www.example.com";
-        const tasks = [];
-        tasks.push(next => {
-          cj.setCookie("broken_path=testme; path=/;", ex, at(-1), next);
+      topic: function () {
+        var cb = this.callback;
+        var cj = new CookieJar();
+        var ex = 'http://www.example.com';
+        var tasks = [];
+        tasks.push(function (next) {
+          cj.setCookie('broken_path=testme; path=/;', ex, at(-1), next);
         });
-        tasks.push(next => {
-          cj.setCookie("b=2; Path=/;;;;", ex, at(-1), next);
+        tasks.push(function (next) {
+          cj.setCookie('b=2; Path=/;;;;', ex, at(-1), next);
         });
-        async.parallel(tasks, (err, cookies) => {
+        async.parallel(tasks, function (err, cookies) {
           cb(null, {
             cj: cj,
             cookies: cookies
           });
         });
       },
-      "check number of cookies": function(t) {
+      "check number of cookies": function (t) {
         assert.lengthOf(t.cookies, 2, "didn't set");
       },
-      "check *broken_path* was set properly": function(t) {
+      "check *broken_path* was set properly": function (t) {
         assert.equal(t.cookies[0].key, "broken_path");
         assert.equal(t.cookies[0].value, "testme");
         assert.equal(t.cookies[0].path, "/");
       },
-      "check *b* was set properly": function(t) {
+      "check *b* was set properly": function (t) {
         assert.equal(t.cookies[1].key, "b");
         assert.equal(t.cookies[1].value, "2");
         assert.equal(t.cookies[1].path, "/");
       },
       "retrieve the cookie": {
-        topic: function(t) {
-          const cb = this.callback;
-          t.cj.getCookies("http://www.example.com", {}, (err, cookies) => {
+        topic: function (t) {
+          var cb = this.callback;
+          t.cj.getCookies('http://www.example.com', {}, function (err, cookies) {
             t.cookies = cookies;
             cb(err, t);
           });
         },
-        "get the cookie": function(t) {
+        "get the cookie": function (t) {
           assert.lengthOf(t.cookies, 2);
-          assert.equal(t.cookies[0].key, "broken_path");
-          assert.equal(t.cookies[0].value, "testme");
+          assert.equal(t.cookies[0].key, 'broken_path');
+          assert.equal(t.cookies[0].value, 'testme');
           assert.equal(t.cookies[1].key, "b");
           assert.equal(t.cookies[1].value, "2");
           assert.equal(t.cookies[1].path, "/");
@@ -131,15 +127,15 @@ vows
   })
   .addBatch({
     "tough-cookie throws exception on malformed URI (GH-32)": {
-      topic: function() {
-        const url = "http://www.example.com/?test=100%";
-        const cj = new CookieJar();
+      topic: function () {
+        var url = "http://www.example.com/?test=100%";
+        var cj = new CookieJar();
 
         cj.setCookieSync("Test=Test", url);
 
         return cj.getCookieStringSync(url);
       },
-      "cookies are set": function(cookieStr) {
+      "cookies are set": function (cookieStr) {
         assert.strictEqual(cookieStr, "Test=Test");
       }
     }
