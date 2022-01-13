0ef85bb3b7342ad3fa9d4b953aad24cbe5b8ae86
Colin Casey <casey.colin@gmail.com>
2022-01-13 14:28:41 -0400

    Update test/node_util_fallback_test.js
    Co-authored-by: Will Harney <62956339+wjhsf@users.noreply.github.com>

--- test/node_util_fallback_test.js
+++ test/node_util_fallback_test.js
@@ -1,5 +1,5 @@
 /*!
- * Copyright (c) 2021, Salesforce.com, Inc.
+ * Copyright (c) 2022, Salesforce.com, Inc.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
