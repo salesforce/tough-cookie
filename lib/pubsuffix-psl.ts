923ce7c3c7df33818f7980b6d39402731a332669
Colin Casey <casey.colin@gmail.com>
2023-12-13 17:08:39 -0400

    Replace `psl` with `tldts`
    One of the topics that came up in discussions for v5 was considering replacements for `psl` (see https://github.com/salesforce/tough-cookie/discussions/255). This PR replaces that library with `tldts` which is more performant.
    
    Fixes #239
    Fixes #338

--- lib/pubsuffix-psl.ts
+++ lib/pubsuffix-psl.ts
@@ -29,7 +29,7 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 'use strict'
-import * as psl from 'psl'
+import { getDomain } from 'tldts'
 
 // RFC 6761
 const SPECIAL_USE_DOMAINS = ['local', 'example', 'invalid', 'localhost', 'test']
@@ -84,5 +84,5 @@ export function getPublicSuffix(
     )
   }
 
-  return psl.get(domain)
+  return getDomain(domain)
 }
