4afc5d6a35d3f51a03efdf1511eb54e813c7eefd
Colin Casey <casey.colin@gmail.com>
2024-02-29 14:09:27 -0400

    Preparing for release 5.0.0-rc.1

--- lib/__tests__/jarSerialization.spec.ts
+++ lib/__tests__/jarSerialization.spec.ts
@@ -37,7 +37,7 @@ import { version } from '../version'
 
 describe('cookieJar serialization', () => {
   it('should use the expected version', () => {
-    expect(version).toBe('5.0.0-rc.0')
+    expect(version).toBe('5.0.0-rc.1')
   })
 
   it('should provide the list of serialized properties available for a Cookie with `Cookie.serializableProperties`', () => {
