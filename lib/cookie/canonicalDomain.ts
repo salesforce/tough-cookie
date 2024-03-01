842e1297926c47d3f7b460af8645114b672d57d4
Will Harney <wharney@salesforce.com>
2024-03-01 14:49:19 -0500

    add explicit return type to canonicalDomain

--- lib/cookie/canonicalDomain.ts
+++ lib/cookie/canonicalDomain.ts
@@ -1,9 +1,9 @@
-import * as punycode from 'punycode/punycode.js'
+import { toASCII } from 'punycode/punycode.js'
 import { IP_V6_REGEX_OBJECT } from './constants'
 import type { Nullable } from '../utils'
 
 // S5.1.2 Canonicalized Host Names
-export function canonicalDomain(str: Nullable<string>) {
+export function canonicalDomain(str: Nullable<string>): string | null {
   if (str == null) {
     return null
   }
@@ -16,7 +16,7 @@ export function canonicalDomain(str: Nullable<string>) {
   // convert to IDN if any non-ASCII characters
   // eslint-disable-next-line no-control-regex
   if (/[^\u0001-\u007f]/.test(_str)) {
-    _str = punycode.toASCII(_str)
+    _str = toASCII(_str)
   }
 
   return _str.toLowerCase()
