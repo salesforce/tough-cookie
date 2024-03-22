921b1aa836db8f8aba79c2a5085207fef3396fbc
Colin Casey <casey.colin@gmail.com>
2024-03-22 09:43:28 -0300

    Document `domainMatch`

--- lib/cookie/domainMatch.ts
+++ lib/cookie/domainMatch.ts
@@ -8,13 +8,48 @@ import { canonicalDomain } from './canonicalDomain'
 const IP_REGEX_LOWERCASE =
   /(?:^(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)(?:\.(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)){3}$)|(?:^(?:(?:[a-f\d]{1,4}:){7}(?:[a-f\d]{1,4}|:)|(?:[a-f\d]{1,4}:){6}(?:(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)(?:\.(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)){3}|:[a-f\d]{1,4}|:)|(?:[a-f\d]{1,4}:){5}(?::(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)(?:\.(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)){3}|(?::[a-f\d]{1,4}){1,2}|:)|(?:[a-f\d]{1,4}:){4}(?:(?::[a-f\d]{1,4}){0,1}:(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)(?:\.(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)){3}|(?::[a-f\d]{1,4}){1,3}|:)|(?:[a-f\d]{1,4}:){3}(?:(?::[a-f\d]{1,4}){0,2}:(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)(?:\.(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)){3}|(?::[a-f\d]{1,4}){1,4}|:)|(?:[a-f\d]{1,4}:){2}(?:(?::[a-f\d]{1,4}){0,3}:(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)(?:\.(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)){3}|(?::[a-f\d]{1,4}){1,5}|:)|(?:[a-f\d]{1,4}:){1}(?:(?::[a-f\d]{1,4}){0,4}:(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)(?:\.(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)){3}|(?::[a-f\d]{1,4}){1,6}|:)|(?::(?:(?::[a-f\d]{1,4}){0,5}:(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)(?:\.(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)){3}|(?::[a-f\d]{1,4}){1,7}|:)))$)/
 
-// S5.1.3 Domain Matching
+/**
+ * Answers "does this real domain match the domain in a cookie?". The `domain` is the "current" domain name and the
+ * `cookieDomain` is the "cookie" domain name. Matches according to {@link https://www.rfc-editor.org/rfc/rfc6265.html#section-5.1.3 | RFC6265 - Section 5.1.3},
+ * but it helps to think of it as a "suffix match".
+ *
+ * @remarks
+ * ### 5.1.3.  Domain Matching
+ *
+ * A string domain-matches a given domain string if at least one of the
+ * following conditions hold:
+ *
+ * - The domain string and the string are identical.  (Note that both
+ *     the domain string and the string will have been canonicalized to
+ *     lower case at this point.)
+ *
+ * - All of the following conditions hold:
+ *
+ *     - The domain string is a suffix of the string.
+ *
+ *     - The last character of the string that is not included in the
+ *         domain string is a %x2E (".") character.
+ *
+ *     - The string is a host name (i.e., not an IP address).
+ *
+ * @example
+ * ```
+ * domainMatch('example.com', 'example.com') === true
+ * domainMatch('eXaMpLe.cOm', 'ExAmPlE.CoM') === true
+ * domainMatch('no.ca', 'yes.ca') === false
+ * ```
+ *
+ * @param domain - The domain string to test
+ * @param cookieDomain - The cookie domain string to match against
+ * @param canonicalize - The canonicalize parameter toggles whether the domain parameters get normalized with canonicalDomain or not
+ * @public
+ */
 export function domainMatch(
-  str?: Nullable<string>,
-  domStr?: Nullable<string>,
+  domain?: Nullable<string>,
+  cookieDomain?: Nullable<string>,
   canonicalize?: boolean,
 ): boolean | undefined {
-  if (str == null || domStr == null) {
+  if (domain == null || cookieDomain == null) {
     return undefined
   }
 
@@ -22,11 +57,11 @@ export function domainMatch(
   let _domStr: Nullable<string>
 
   if (canonicalize !== false) {
-    _str = canonicalDomain(str)
-    _domStr = canonicalDomain(domStr)
+    _str = canonicalDomain(domain)
+    _domStr = canonicalDomain(cookieDomain)
   } else {
-    _str = str
-    _domStr = domStr
+    _str = domain
+    _domStr = cookieDomain
   }
 
   if (_str == null || _domStr == null) {
@@ -49,7 +84,7 @@ export function domainMatch(
   /* " o All of the following [three] conditions hold:" */
 
   /* "* The domain string is a suffix of the string" */
-  const idx = _str.lastIndexOf(domStr)
+  const idx = _str.lastIndexOf(cookieDomain)
   if (idx <= 0) {
     return false // it's a non-match (-1) or prefix (0)
   }
