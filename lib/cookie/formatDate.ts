b72a94d9a6a9089c4f144d24c5b1d0c0101f59a2
Colin Casey <casey.colin@gmail.com>
2024-03-23 19:01:50 -0300

    Document `formatDate`

--- lib/cookie/formatDate.ts
+++ lib/cookie/formatDate.ts
@@ -1,7 +1,20 @@
 import * as validators from '../validators'
 import { safeToString } from '../utils'
 
-/** Converts a Date to a UTC string representation. */
+/**
+ * Format a {@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date | Date} into
+ * the {@link https://www.rfc-editor.org/rfc/rfc2616#section-3.3.1 | preferred Internet standard format}
+ * defined in {@link https://www.rfc-editor.org/rfc/rfc822#section-5 | RFC822} and
+ * updated in {@link https://www.rfc-editor.org/rfc/rfc1123#page-55 | RFC1123}.
+ *
+ * @example
+ * ```
+ * formatDate(new Date(0)) === 'Thu, 01 Jan 1970 00:00:00 GMT`
+ * ```
+ *
+ * @param date - the date value to format
+ * @public
+ */
 export function formatDate(date: Date): string {
   validators.validate(validators.isDate(date), safeToString(date))
   return date.toUTCString()
