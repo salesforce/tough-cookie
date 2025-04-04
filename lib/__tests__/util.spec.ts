c188fb690ddd3e73c2c66e48af38eb09558611df
Will Harney <wharney@salesforce.com>
2025-04-04 12:42:20 -0400

    test(util): add labels to test
    implicit cast to string breaks

--- lib/__tests__/util.spec.ts
+++ lib/__tests__/util.spec.ts
@@ -2,32 +2,35 @@ import { describe, expect, it } from 'vitest'
 import { safeToString } from '../utils.js'
 
 describe('safeToString', () => {
+  const recursiveArray: unknown[] = [1]
+  recursiveArray.push([[recursiveArray], 2, [[recursiveArray]]], 3)
+
   const testCases = [
-    [undefined, 'undefined'],
-    [null, 'null'],
-    [true, 'true'],
-    ['string', 'string'],
-    [123, '123'],
-    [321n, '321'],
-    [{ object: 'yes' }, '[object Object]'],
-    [(a: number, b: number): number => a + b, '(a, b) => a + b'],
-    [Symbol('safeToString'), 'Symbol(safeToString)'],
-    [Object.create(null), '[object Object]'],
-    // eslint-disable-next-line no-sparse-arrays
-    [[1, 'hello', , undefined, , true, null], '1,hello,,,,true,'],
+    ['undefined', undefined, 'undefined'],
+    ['null', null, 'null'],
+    ['boolean', true, 'true'],
+    ['string', 'string', 'string'],
+    ['number', 123, '123'],
+    ['bigint', 321n, '321'],
+    ['object', { object: 'yes' }, '[object Object]'],
+    ['function', (a: number, b: number): number => a + b, '(a, b) => a + b'],
+    ['symbol', Symbol('safeToString'), 'Symbol(safeToString)'],
+    ['null object', Object.create(null), '[object Object]'],
     [
+      'array with primitives',
+      // eslint-disable-next-line no-sparse-arrays
+      [1, 'hello', , undefined, , true, null],
+      '1,hello,,,,true,',
+    ],
+    [
+      'array with object/symbol',
       [Object.create(null), Symbol('safeToString')],
       '[object Object],Symbol(safeToString)',
     ],
+    ['recursive array', recursiveArray, '1,,2,,3'],
   ]
 
-  it.each(testCases)('works on %s', (input, output) => {
-    expect(safeToString(input)).toBe(String(output))
-  })
-
-  it('works on recursive array', () => {
-    const recursiveArray: unknown[] = [1]
-    recursiveArray.push([[recursiveArray], 2, [[recursiveArray]]], 3)
-    expect(safeToString(recursiveArray)).toBe('1,,2,,3')
+  it.each(testCases)('works on %s', (_label, input, output) => {
+    expect(safeToString(input)).toBe(output)
   })
 })
