c5f7706d76cba59919e4576bcac8adc126f68c9e
Will Harney <62956339+wjhsf@users.noreply.github.com>
2025-12-12 19:28:19 +0000

    test(timing): retry flaky test and increase threshold (#552)

--- lib/__tests__/parse.spec.ts
+++ lib/__tests__/parse.spec.ts
@@ -440,6 +440,7 @@ describe('Cookie.parse', () => {
     },
   ])(
     'Cookie.parse("$prefix $postfix") should not take significantly longer to run than Cookie.parse("$prefix<TOO MANY SPACES>$postfix")',
+    { retry: 3 },
     ({ prefix, postfix, parseOptions = {} }) => {
       const shortVersion = `${prefix} ${postfix}`
       const startShortVersionParse = performance.now()
@@ -454,7 +455,7 @@ describe('Cookie.parse', () => {
       const ratio =
         (endLongVersionParse - startLongVersionParse) /
         (endShortVersionParse - startShortVersionParse)
-      expect(ratio).toBeLessThan(250) // if broken this ratio goes 2000-4000x higher
+      expect(ratio).toBeLessThan(300) // if broken this ratio goes 2000-4000x higher
     },
   )
 })
